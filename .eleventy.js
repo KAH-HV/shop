7a64f4c15bb75d4d734120643299f1251ffd18b7
Stefan Frede <stefan@frede.dev>
2020-05-19 08:41:28 +0200

    refactor: reorder folder structure

--- .eleventy.js
+++ .eleventy.js
@@ -16,9 +16,8 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: "src/site",
-      includes: "_includes",
-      output: "src/dist",
+      input: "src",
+      output: "src/_output",
     },
   };
 };
